c54a94d4674279e2b55bcd764ddca55e023d8be2
togregoire <gregoiretommy@gmail.com>
2014-03-26 13:00:10 -0400

    board print

--- SudokuSolver.cpp
+++ SudokuSolver.cpp
@@ -2,30 +2,62 @@
 #include <fstream>
 #include <cmath>
 
+#define SUDOKU_SIZE 9
+
 using namespace std;
 
-int sudokuBoard[9][9];
+int sudokuBoard[SUDOKU_SIZE][SUDOKU_SIZE];
+int un = 0;
 
-int main(){
-	int un = 0,r,c,tmp;
+void printBoard();
+void fillBoard();
+
+int main() {
+	int r,c,tmp;
 	bool b = true;
-	const char* filename = "sudoku";
+
+	fillBoard();
+
+	printBoard();
+	
+	return 0;
+}
+
+/* Function to fill the board */
+void fillBoard() {
+	const char* filename = "sudoku.txt";
 	ifstream input(filename);
 	
-	if(!input.isopen())
-	{
+	if (!input.is_open()) {
 		cout << "Problem with the file\n";
-		return 0;
+		return;
 	}
 	
-	for(int i=0;i<9;++i)
-		for(int j=0;j<9;++j)
-		{
+	for (int i = 0; i < SUDOKU_SIZE; ++i)
+		for (int j = 0; j < SUDOKU_SIZE; ++j) {
 			input >> sudokuBoard[i][j];
-			if(!sudokuBoard[i][j])
+			if (!sudokuBoard[i][j])
 				un++;
 		}
 	input.close();
-	
-	return 0;
+}
+
+/* Function to print the board */
+void printBoard() {
+	cout << "+-------+-------+-------+\n";
+
+	for (int i = 0; i < SUDOKU_SIZE; ++i) {
+		cout << "| ";
+		for (int j = 0; j < SUDOKU_SIZE; ++j) {
+			if (sudokuBoard[i][j] != 0) 
+				cout << sudokuBoard[i][j];
+			else
+				cout << ".";
+			cout << " ";
+			if ((j + 1) % 3 == 0) cout << "| ";
+		}
+		if ((i + 1) % 3 == 0)
+			cout << "\n+-------+-------+-------+";
+		cout << "\n";
+	}
 }
