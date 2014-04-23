f0526c289e51d0ac7663e85fe0f70f595299e2c2
togregoire <gregoiretommy@gmail.com>
2014-04-23 17:04:14 -0400

    restructuration

---
+++ Parallel/makeLinux.sh
@@ -0,0 +1,1 @@
+g++ -o sudoku src/main.cc -pthread src/Sudoku.h src/Sudoku.cc src/SubGrid.h src/SubGrid.cc src/solver.h src/solver.cc
--- Parallel/makeSolaris.sh
+++
@@ -1,1 +0,0 @@
-g++ -o sudoku src/main.cc -lm -lpthread src/Sudoku.h src/Sudoku.cc src/SubGrid.h src/SubGrid.cc src/solver.h src/solver.cc
